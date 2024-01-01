e0e4ddef10145be030d9fcc6fdabd3fe6c62a24d
AxiomOfChoices <reznyakov@gmail.com>
2024-01-01 18:32:13 -0500

    Fixed compilation of background images

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,36 +1,49 @@
 <script>
-	import Header from './Header.svelte';
-	import './styles.css';
+  import Header from "./Header.svelte";
+  import "./styles.css";
+  import background_tile_png from "$lib/images/background_tile.png";
+  import background_tile from "$lib/images/background_tile.webp";
 </script>
 
-<div class="app">
-	<Header />
+<div
+  class="app"
+  style="background-image:
+    image-set(
+      url({background_tile_png}) type(&quot;image/png&quot;),
+      url({background_tile}) type(&quot;image/webp&quot;)
+    );"
+>
+  <Header />
 
-	<main>
-		<slot />
-	</main>
+  <main>
+    <slot />
+  </main>
 </div>
 
 <style>
-	.app {
-		display: flex;
-		flex-direction: column;
-		height: 100vh;
-	}
+  .app {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
 
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		place-content: center;
-		width: 100%;
-		padding: 0% 3%;
-		margin: 0 auto;
-		height: calc(100% - 3em);
-		box-sizing: border-box;
-	}
+    background-color: var(--main-color);
+    background-repeat: repeat;
+    background-size: 15%;
+  }
 
-	/* footer {
+  main {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    place-content: center;
+    width: 100%;
+    padding: 0% 3%;
+    margin: 0 auto;
+    height: calc(100% - 3em);
+    box-sizing: border-box;
+  }
+
+  /* footer {
 		display: flex;
 		flex-direction: column;
 		justify-content: center;
@@ -42,7 +55,7 @@
 		font-weight: bold;
 	} */
 
-	/* @media (min-width: 480px) {
+  /* @media (min-width: 480px) {
 		footer {
 			padding: 12px 0;
 		}
--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,11 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	build:{
-		target: [ "es2020", "safari13" ]
-	}
+  plugins: [sveltekit()],
+  build: {
+    target: ["es2020", "safari13"]
+  },
+  server: {
+    host: "0.0.0.0"
+  }
 });
